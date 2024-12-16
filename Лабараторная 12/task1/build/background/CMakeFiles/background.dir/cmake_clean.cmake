74bd47bd1cc16f796c9982578227cba980717854
Nikitulka0120 <107490259+Nikitulka0120@users.noreply.github.com>
2024-12-16 17:18:39 +0700

    Add files via upload

---
+++ Лабараторная 12/task1/build/background/CMakeFiles/background.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/background.dir/background.c.o"
+  "CMakeFiles/background.dir/background.c.o.d"
+  "libbackground.pdb"
+  "libbackground.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/background.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
